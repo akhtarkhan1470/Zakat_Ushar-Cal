585d5994a442233322b6a044c0a4a043a80ddc14
Muhammad Akhtar <akhtarkhanuol15@gmail.com>
2025-01-21 14:47:33 +0500

    Add files via upload

---
+++ Zakat_Ushar js/index.js
@@ -0,0 +1,16 @@
+// zakat calculation
+alert("zakat ratio is 2.5% of total income")
+totalincome=prompt("Enter your Income")
+result=totalincome * 0.025
+alert(result)
+document.write("Zakat :", result, "<br>" )
+console.log("Zakat: =", result, "<br>" )
+
+
+// ushar calculation
+
+alert("natural resource rate is 10%")
+naturalresorces=prompt("Enter your natural resourcevalue")
+result=naturalresorces* 10/100
+alert(result)
+document.write("ushar: =" ,result ,"<br>");
